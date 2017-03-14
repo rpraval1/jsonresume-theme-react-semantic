8a983af44c299275cad5218adb2335dc7d9afc63
Pravallika Rikhala <rpraval1@binghamton.edu>
2017-03-14 00:34:59 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-resume
+React and Semantic based App using https://jsonresume.org/schema/ open source schema
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Container, Divider } from 'semantic-ui-react'
+
+import './css/App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+    }
+  }
+
+
+  render() {
+
+    return (
+      <Container fluid>
+
+      </Container>
+    );
+  }
+}
+
+export default App;
